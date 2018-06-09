afabbed2adadd70683f76d8fdd786678e6756c19
Ansel Adams <dj@volaciousbrands.com>
2018-06-09 13:40:05 -0500

    working logic

--- javascript/app.js
+++ javascript/app.js
@@ -3,16 +3,26 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 var searchTerm;
 
-url += '?' + $.param({
-  'api-key': "be69431c492a4ed5ac4fc06c5f62ac03",
-  'q': searchTerm
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).then(function(result) {
-  console.log(result);
-  console.log(result.response.docs[2].headline);
-}).fail(function(err) {
-  throw err;
-});
+$(".btn").on("click", function() {
+  searchTerm = $("#exampleFormControlInput1").val();
+  console.log(searchTerm);
+
+  url += '?' + $.param({
+    'api-key': "be69431c492a4ed5ac4fc06c5f62ac03",
+    'q': searchTerm
+  });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).then(function(result) {
+    console.log(result);
+    // console.log(result.response.docs[2].headline);
+  }).fail(function(err) {
+    throw err;
+  });
+
+  var resultsDiv = $("<div>");
+
+  $("body").append(resultsDiv.html("hi"));
+})
+
